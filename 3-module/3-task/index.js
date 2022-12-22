2bf897df062a4553d8128da11324eacff2ac7627
Ekaterina <ekaterina.alex.nana@gmail.com>
2022-12-22 22:02:36 +0300

    edited 3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,13 +1,12 @@
 function camelize(str) {
-  let arr = str.split('-');
-  
-  for(i = 1; i < arr.length; i++) {
-    const char = arr[i].slice(0,1).toUpperCase();
-    const restChars = arr[i].slice(1,arr[i].length);
-    arr[i] = char + restChars;
-  }
-  
-  let strFromArr = arr.join(""); 
-  
-  return strFromArr
+  return str.split('-')
+            .map((item, index) => {
+                if (index >= 1) {
+                  return item = item.slice(0,1).toUpperCase() + item.slice(1,item.length);
+                }
+
+                return item
+            })
+            .join(""); 
+
 }
